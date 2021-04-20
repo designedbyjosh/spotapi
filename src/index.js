17a86f41fc8e382636a8ed157eed79977fabb506
Josh <57708928+designedbyjosh@users.noreply.github.com>
2021-04-20 23:49:27 +1000

    🎉 Added version 1.3

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
     cors: {
       origin: '*',
+      methods: ["GET", "POST"],
+      allowedHeaders: ["content-type"],
     },
     path: "/ws"
   })
